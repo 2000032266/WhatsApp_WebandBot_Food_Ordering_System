8febecd0b6f470aaa5afcdf963ddf259f6c6688b
2000032266 <2000032266@kluniversity.in>
2025-07-23 11:46:56 +0530

    Remove all files from repository

--- backend/utils/notificationServer.js
+++
@@ -1,8 +0,0 @@
-// WebSocket notification server removed. No-op exports for compatibility.
-module.exports = {
-  initialize: () => {},
-  sendToUser: () => {},
-  broadcast: () => {},
-  getConnectedUsers: () => [],
-  handlePing: () => {}
-};
